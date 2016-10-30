04e52b978c69747fab147167a5b2d5c177d431fe
Adam Linkous <alinkous@gmail.com>
2016-10-30 17:01:18 -0500

    install docker
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# add the yum repo
+cookbook_file '/etc/yum.repos.d/docker.repo' do
+  source 'docker.repo'
+  action :create
+end
+
+# install docker
+package 'docker-engine' do
+  action :install
+end
+
+# enable and start docker service
+service 'docker' do
+  action [:enable, :start]
+end
