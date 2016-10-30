d67d5051432a96899ea03c9c448abb85a9183e51
Adam Linkous <alinkous@gmail.com>
2016-10-30 16:06:26 -0500

    generate chef cookbook scaffolding
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'dockerserver'
+maintainer 'Adam Linkous'
+maintainer_email 'alinkous+support@gmail.com'
+license 'MIT License'
+description 'Installs/Configures dockerserver'
+long_description 'Installs/Configures dockerserver'
+version '0.1.0'
+issues_url 'https://github.com/gryte/dockerserver/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/gryte/dockerserver' if respond_to?(:source_url)
