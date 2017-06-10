7261bc96d27354f6100e6ec9b7b56ac8f9c6914a
Adam Linkous <alinkous@gmail.com>
2017-06-10 18:12:11 -0500

    add firewalld tests
    - add inspec tests
    - add rubocop.yml file to manage line endings
    - update metadata file to appease foodcritic checks
    - bump version
    - update kitchen yml
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -19,3 +19,15 @@ end
 describe package('nfs-utils') do
   it { should be_installed }
 end
+
+# firewalld service is enabled and running
+describe service('firewalld') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+# iptables is configured
+describe iptables(chain: 'INPUT_direct') do
+  it { should have_rule('-A INPUT_direct -p tcp -m tcp -m multiport --dports 22 -m comment --comment ssh -j ACCEPT') }
+end
