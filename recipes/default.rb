1ab45257aef545ac3bc706a0905a7d69071885f2
adam linkous <alinkous@gmail.com>
2020-07-30 21:59:06 -0500

    Add More K3s Ports

--- recipes/default.rb
+++ recipes/default.rb
@@ -47,23 +47,93 @@ firewall_rule 'ssh' do
 end
 
 # open ports for Kubernetes API (K3s server nodes)
-firewall_rule 'plex-htc-cmpn' do
+firewall_rule 'kubernetes-api' do
   protocol :tcp
   port 6443
   command :allow
 end
 
 # open ports for Flannel VXLAN (K3s server and agent nodes)
-firewall_rule 'bonjour-avahi' do
+firewall_rule 'flannel-vxlan' do
   protocol :udp
   port 8472
   command :allow
 end
 
 # open ports for kubelet (K3s server and agent nodes)
-firewall_rule 'plex-htc-cmpn' do
+firewall_rule 'kubelet' do
   protocol :tcp
   port 10250
+  command :allow
+end
+
+# open ports for Node driver Docker daemon TLS port
+firewall_rule 'node-driver-docker-daemon-tls-port' do
+  protocol :tcp
+  port 2376
+  command :allow
+end
+
+# open ports for etcd client requests
+firewall_rule 'etcd-client-requests' do
+  protocol :tcp
+  port 2379
+  command :allow
+end
+
+# open ports for etcd peer communication
+firewall_rule 'etcd-peer-communication' do
+  protocol :tcp
+  port 2380
+  command :allow
+end
+
+# open ports for Flannel livenessProbe/readinessProbe
+firewall_rule 'flannel-livenessProbe-readinessProbe' do
+  protocol :tcp
+  port 9099
+  command :allow
+end
+
+# open ports for Default port required by Monitoring to scrape metrics
+firewall_rule 'monitoring-scrape-metrics' do
+  protocol :tcp
+  port 9796
+  command :allow
+end
+
+# open ports for Weave Port
+firewall_rule 'weave-port' do
+  protocol :tcp
+  port 6783
+  command :allow
+end
+
+# open ports for Weave UDP Ports
+firewall_rule 'weave-udp-ports' do
+  protocol :udp
+  port 6783..6784
+  command :allow
+end
+
+# open ports for Ingress controller livenessProbe/readinessProbe
+firewall_rule 'ingress-controller-livenessProbe-readinessProbe' do
+  protocol :tcp
+  port 10254
+  command :allow
+end
+
+# open ports for NodePort port range TCP
+firewall_rule 'nodeport-port-range-tcp' do
+  protocol :tcp
+  port 30000..32767
+  command :allow
+end
+
+# open ports for NodePort port range UDP
+firewall_rule 'nodeport-port-range-udp' do
+  protocol :udp
+  port 30000..32767
   command :allow
 end
 
